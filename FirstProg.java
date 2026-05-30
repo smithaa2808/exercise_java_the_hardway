cfd82973e9a912062023f0aae90b32cc3328f0c1
smithaa2808 <smithaathirunavu@gmail.com>
2026-05-30 20:37:06 +0530

    second commit with 14th exercise

--- FirstProg.java
+++ FirstProg.java
@@ -3,7 +3,7 @@ public class FirstProg
     public static void main( String[] args )
     {
         System.out.println( "I am determined to learn how to code." );
-        // This is a comment 
+        // This is comment I added on 30th May
         System.out.println( "Today's date is" );
     }
 }
