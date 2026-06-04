066d126b02ec5e7b4f3b918d18cf5348588709aa
smithaa2808 <smithaathirunavu@gmail.com>
2026-06-04 15:36:03 +0530

    21th commit

---
+++ OverlyComplexFlag.java
@@ -0,0 +1,77 @@
+import static java.lang.System.*;
+
+public class OverlyComplexFlag
+{
+    public static void main( String[] args )
+    {
+        printTopHalf();
+
+        print48Colons();
+        print48Ohs();
+        print48Colons();
+        print48Ohs();
+        print48Colons();
+        print48Ohs();
+    }
+
+    public static void print48Colons()
+    // print48Colons is a method 
+    {
+        out.println( "|::::::::::::::::::::::::::::::::::::::::::::::::|" );
+    }
+
+    public static void print48Ohs()
+    {
+        out.println( "|OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO|" );
+    }
+
+    public static void print29Colons()
+    {
+        out.println( "|:::::::::::::::::::::::::::::|" );
+    }
+
+    public static void printPledge()
+    {
+        out.println( "I pledge allegiance to the flag." );
+    }
+
+    public static void print29Ohs()
+    {
+        out.println( "|OOOOOOOOOOOOOOOOOOOOOOOOOOOOO|" );
+    }
+
+    public static void print6Stars()
+    {
+        out.print( "| *  *  *  *  *  * " );
+    }
+
+    public static void print5Stars()
+    {
+        out.print( "|   *  *  *  *  *  " );
+    }
+
+    public static void printSixStarLine()
+    {
+        print6Stars();
+        print29Ohs();
+    }
+
+    public static void printFiveStarLine()
+    {
+        print5Stars();
+        print29Colons();
+    }
+
+    public static void printTopHalf()
+    {
+        out.println( " ________________________________________________" ); //
+        printSixStarLine();
+        printFiveStarLine();
+        printSixStarLine();
+        printFiveStarLine();
+        printSixStarLine();
+        printFiveStarLine();
+        printSixStarLine();
+    }
+
+}
