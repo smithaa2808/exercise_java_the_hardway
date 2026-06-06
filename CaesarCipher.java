8e5ffd04846285f3d12aa5a4a83454c81a8ab531
smithaa2808 <smithaathirunavu@gmail.com>
2026-06-06 13:42:05 +0530

    33rd commit confused with shiftLetter

---
+++ CaesarCipher.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+ 
+public class CaesarCipher
+{
+    /**
+     * Returns the character shifted by the given number of letters.
+     */
+    public static char shiftLetter( char c, int n )
+    {
+        int ch = c; // This will convert the char to its unicode 
+         if ( ! Character.isLetter(c) )
+            return c;
+         ch = ch + n;
+        if ( Character.isUpperCase(c) && ch > 'Z' || Character.isLowerCase(c) && ch > 'z' )
+            ch = 26;
+        if ( Character.isUpperCase(c) && ch < 'A' || Character.isLowerCase(c) && ch < 'a' )
+            ch += 26;
+         return (char)ch;
+    }
+     public static void main( String[] args )
+    {
+        Scanner keyboard = new Scanner(System.in);
+        String plaintext, cipher = ""; // empty string 
+
+        int shift;
+         System.out.print("Message: ");
+        plaintext = keyboard.nextLine();
+        System.out.print("Shift (0-26): ");
+        shift = keyboard.nextInt();
+         for ( int i=0; i<plaintext.length(); i++ )
+        {
+            cipher += shiftLetter( plaintext.charAt(i), shift );
+            //  charAt(1) it will give the corresponding element of the given index 
+        }
+        System.out.println( cipher );
+     }
+}
