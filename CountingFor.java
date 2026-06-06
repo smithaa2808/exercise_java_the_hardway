f9b403da2aba5053349b1dd680b5382fdb19f996
smithaa2808 <smithaathirunavu@gmail.com>
2026-06-06 13:01:06 +0530

    32nd commit

---
+++ CountingFor.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class CountingFor
+{
+    public static void main( String[] args )
+    {
+        Scanner keyboard = new Scanner(System.in);
+
+        int n;
+        String message;
+
+        System.out.println( "Type in a message, and I'll display it five times." );
+        System.out.print( "Message: " );
+        message = keyboard.nextLine();
+
+        for ( n=1 ; n <= 5 ; n++ )
+        {
+            System.out.println( n + ". " + message );
+        }
+
+        System.out.println( "\nNow I'll display it ten times and count by 5s.");
+        for ( n = 5 ; n <= 50 ; n += 5 ) // n=n+5
+        {
+            System.out.println( n + ". " + message );
+        }
+
+        System.out.println( "\nFinally, three times counting backward." );
+        for ( n = 3 ; n > 0 ; n-- )
+        {
+            System.out.println( n + ". " + message );
+        }
+
+    }
+}
