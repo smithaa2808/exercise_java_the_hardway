9e34e530057f9453936ff7837c76c871441c18d9
smithaa2808 <smithaathirunavu@gmail.com>
2026-06-06 17:55:57 +0530

    36th commit

---
+++ FarmerBrownWhile.java
@@ -0,0 +1,27 @@
+public class FarmerBrownWhile
+{
+    public static void main( String[] args )
+    {
+        int s = 1;
+        while ( s <= 100 )
+        {
+            int g = 1;
+            while ( g <= 100 )
+            {
+                int c = 1;
+                while ( c <= 100 )
+                {
+                    if ( s+g+c == 100 && 10.00*s + 3.50*g + 0.50*c == 100.00 )
+                    {
+                        System.out.print( s + " sheep, " );
+                        System.out.print( g + " goats, and " );
+                        System.out.println( c + " chickens." );
+                    }
+                    c++;
+                }
+                g++;
+            }
+            s++;
+        }
+    }
+}
