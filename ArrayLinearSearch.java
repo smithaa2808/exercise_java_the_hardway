b7827be772e6fcf9fc8760d2c077d6c9ff19ef54
smithaa2808 <smithaathirunavu@gmail.com>
2026-06-08 15:15:16 +0530

    39th commit

---
+++ ArrayLinearSearch.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class ArrayLinearSearch
+{
+    public static void main( String[] args )
+    {
+        Scanner keyboard = new Scanner(System.in);
+
+        int[] orderNumbers = { 12345, 54321, 78753, 101010, 8675309, 31415, 271828 }; //creating array 
+        int toFind;
+
+        System.out.println("There are " + orderNumbers.length + " orders in the database.");
+        // orderNumber.length will give the size of the array
+
+        System.out.print("Orders: ");
+        for ( int num : orderNumbers )
+        {
+            System.out.print( num + "  " );
+        }
+        System.out.println();
+
+        System.out.print( "Which order to find? " );
+        toFind = keyboard.nextInt();
+
+        for ( int num : orderNumbers )
+        {
+            if ( num == toFind )
+            {
+                System.out.println( num + " found.");
+            }
+
+        // else
+            // {
+            //     System.out.println("The number not found");
+            // }
+        }
+    }
+}
